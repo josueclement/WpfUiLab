30532adcc135d07e8f656c135ae09119d68497f0
Josué Clément <josue.d.clement@gmail.com>
2024-10-07 21:51:56 +0200

    Add WPF-UI and Hosting libraries

--- WpfUiLab/App.xaml.cs
+++ WpfUiLab/App.xaml.cs
@@ -1,12 +1,45 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WpfUiLab;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
+    // ReSharper disable once InconsistentNaming
+    private static readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureServices((_1, services) =>
+        {
+            _ = services.AddSingleton<MainWindow>();
+        })
+        .Build();
+
+    /// <inheritdoc />
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+        
+        GetRequiredService<MainWindow>().Show();
+    }
+
+    /// <inheritdoc />
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.StopAsync().Wait();
+        _host.Dispose();
+    }
+    
+    /// <summary>
+    /// Gets registered service.
+    /// </summary>
+    /// <typeparam name="T">Type of the service to get.</typeparam>
+    /// <returns>Instance of the service or <see langword="null"/>.</returns>
+    public static T GetRequiredService<T>()
+        where T : class
+    {
+        return _host.Services.GetRequiredService<T>();
+    }
 }
--- WpfUiLab/MainWindow.xaml.cs
+++ WpfUiLab/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -14,10 +15,15 @@ namespace WpfUiLab;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        // Wpf.Ui.Appearance.ApplicationThemeManager.Apply(
+        //     Wpf.Ui.Appearance.ApplicationTheme.Dark, // Theme type
+        //     Wpf.Ui.Controls.WindowBackdropType.Mica,  // Background type
+        //     true                                      // Whether to change accents automatically
+        // );
     }
 }
