9d00431fe2194ad7e4136c09ce3d847653cb9ead
Josué Clément <josue.d.clement@gmail.com>
2024-10-08 13:38:41 +0200

    Add test content and navigation

--- WpfUiLab/ViewModels/Pages/HomePageViewModel.cs
+++ WpfUiLab/ViewModels/Pages/HomePageViewModel.cs
@@ -1,8 +1,26 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.Ui;
+using WpfUiLab.Views.Pages;
 
 namespace WpfUiLab.ViewModels.Pages;
 
 public class HomePageViewModel : ObservableObject
 {
+    private readonly INavigationService _navigationService;
+
+    public HomePageViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+        GoToSettingsCommand = new AsyncRelayCommand(GoToSettings);
+        
+    }
     
+    public AsyncRelayCommand GoToSettingsCommand { get; }
+
+    private async Task GoToSettings()
+    {
+        _navigationService.Navigate(typeof(SettingsPage));
+    }
 }
